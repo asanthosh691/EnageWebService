29c0cff57b53ffc6fbf1ee41d698d25e155b4fd8
DESKTOP-F2L6QB8\raj_h91 <santhosh.yellamraju@valuelabs.com>
2020-10-01 15:30:30 +0530

    Added new

---
+++ ProjectInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace EnageWebService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+            base.OnAfterInstall(savedState);
+            
+            using(System.ServiceProcess.ServiceController controller= new ServiceController(serviceInstaller1.ServiceName))
+            {
+                controller.Start();
+            }
+        }
+    }
+}
